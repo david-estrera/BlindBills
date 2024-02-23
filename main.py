49e5347f79a0b41ffc52c9910253ecf3f8771f95
Miguel <93895318+bendItLikeBeckham@users.noreply.github.com>
2024-02-23 22:36:43 +0800

    file rename

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import time
 from cvzone.ColorModule import ColorFinder
 from image_cluster import ImageClusterer
 # set up camera
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(0)
 cap.set(3,640)
 cap.set(4, 480)
 
